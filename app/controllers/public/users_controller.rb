5100cbaaa808551a776e05014e38d7a85f44cda7
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-07-13 12:40:56 +0000

    Fix devise

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,8 +1,8 @@
 class Public::UsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
-  
+  before_action :is_matching_login_user, only: [:show, :edit, :update, :check]
   def show
-    
+    @user = current_user
   end
 
   def edit
@@ -21,11 +21,17 @@ class Public::UsersController < ApplicationController
 
   private
 
-
   def ensure_guest_user
     @user = User.find(params[:id])
     if @user.guest_user?
       redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
     end
   end
 end
