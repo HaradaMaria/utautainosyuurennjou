99b9d2642eaf8e5fdb026483273a8521c04416a0
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-07-27 04:57:43 +0000

    Add Sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ Admin.create!(
   email: "syuurennjou@admin",
   password: "114514"
 )
+
+require "csv"
+
+CSV.foreach('db/usersample.csv') do |row|
+  User.create(:id => row[0], :name => row[1], :email => row[2], :password => row[3], :password_confirmation => row[3], :is_training => row[4])
+end
+
+CSV.foreach('db/songsample.csv') do |row|
+  Song.create(:id => row[0], :name => row[1], :name_kana => row[2], :artist => row[3], :songwriter => row[4], :composer => row[5], :tie_up => row[6])
+end
+
+CSV.foreach('db/recordsample.csv') do |row|
+  Record.create(:user_id => row[0], :song_id => row[1], :score => row[2], :key => row[3], :share => row[4], :memo => row[5], :created_at => row[6], :updated_at => row[7])
+end
